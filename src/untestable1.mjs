d5977437d0bef4372e420eb97b5535fce9e8be92
aaartyomsss <aaartyomsss@gmail.com>
2024-03-27 20:51:33 +0200

    Add comment

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,5 +1,13 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
 
+/**
+ * This code is untestable due to the Date comparison starting from now.
+ * I believe in other word the function is not pure, because each call will 
+ * produce different output.
+ * 
+ * As a result the function should accept specific date as a argument and perform
+ * the calculation for it. Then the code will return the same result each time.
+ */
 export function daysUntilChristmas() {
   const now = new Date();
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
