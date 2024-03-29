5a664873c0ad6bb47b9e92f2d25fc6b7cd356239
aaartyomsss <aaartyomsss@gmail.com>
2024-03-29 18:37:46 +0200

    Fix untestable 2

--- src/untestable2.mjs
+++ src/untestable2.mjs
@@ -4,6 +4,15 @@ function diceRoll() {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
+/**
+ * Nothing can be done really with the function. 
+ * It is just a random function that should and its 
+ * tests should be property based. For instance, we 
+ * could check that it is consistently returns over the period
+ * of time (rolls) all the possible values: [1, 2, 3, 4, 5, 6, 101, 102, 103, 104, 105, 106].
+ * We could also of course change the random roll algorithm to the ShuffleBag or use faker.random.seed,
+ * yet for the dice roll it functionality it feels a bit inappropriate 
+ */
 export function diceHandValue() {
   const die1 = diceRoll();
   const die2 = diceRoll();
--- test/untestable2.test.mjs
+++ test/untestable2.test.mjs
@@ -1,10 +1,43 @@
-import { describe, test } from "vitest";
-import { expect } from "chai";
+import { describe, test, expect  } from "vitest";
 import { diceHandValue } from "../src/untestable2.mjs";
 
 describe("Untestable 2: a dice game", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(diceHandValue()).to.be.a("number");
+
+  const possibleValues = [1, 2, 3, 4, 5, 6, 101, 102, 103, 104, 105, 106]
+
+  test("Returns a single possible value", () => {
+    const value = diceHandValue()
+    expect(possibleValues).to.include(value);
   });
+
+  test("Returns valid values after a round of testing", () => {
+    const retunredValues = []
+    for (let i = 0; i < 1000; i++) {
+      retunredValues.push(diceHandValue())
+    }
+
+    expect(possibleValues).toEqual(expect.arrayContaining(retunredValues))
+  })
+
+  /**
+   * This is rather a bad test, due to still an astronomically low chance 
+   * of one number not poping up after a 100 000 rolls. Though, I believe it is
+   * worth the risk. 
+   */
+  test("Returns valid values after a round of testing at least once", () => {
+    const retunredValuesCount = {}
+
+    possibleValues.forEach(v => {
+      retunredValuesCount[v] = 0
+    })
+
+    for (let i = 0; i < 100000; i++) {
+      const value = diceHandValue()
+      retunredValuesCount[value] += 1
+    }
+
+    possibleValues.forEach(v => {
+      expect(v).toBeGreaterThanOrEqual(1)
+    }) 
+  })
 });
