20e5841a0f02ccb01c796f713834c8739241c844
aaartyomsss <aaartyomsss@gmail.com>
2024-03-29 18:04:26 +0200

    Make lvl 1 testable

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -8,13 +8,24 @@ const millisPerDay = 24 * 60 * 60 * 1000;
  * As a result the function should accept specific date as a argument and perform
  * the calculation for it. Then the code will return the same result each time.
  */
-export function daysUntilChristmas() {
-  const now = new Date();
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
-  if (today.getTime() > christmasDay.getTime()) {
+// export function daysUntilChristmas() {
+//   const now = new Date();
+//   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+//   const christmasDay = new Date(now.getFullYear(), 12 - 1, 25);
+//   if (today.getTime() > christmasDay.getTime()) {
+//     christmasDay.setFullYear(new Date().getFullYear() + 1);
+//   }
+//   const diffMillis = christmasDay.getTime() - today.getTime();
+//   return Math.floor(diffMillis / millisPerDay);
+// }
+
+
+
+export function daysUntilChristmas(date) {
+  const christmasDay = new Date(date.getFullYear(), 12 - 1, 25);
+  if (date.getTime() > christmasDay.getTime()) {
     christmasDay.setFullYear(new Date().getFullYear() + 1);
   }
-  const diffMillis = christmasDay.getTime() - today.getTime();
+  const diffMillis = christmasDay.getTime() - date.getTime();
   return Math.floor(diffMillis / millisPerDay);
 }
--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,8 +3,29 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("todo", () => {
-    // TODO: write proper tests
-    expect(daysUntilChristmas()).to.be.a("number");
+  test("One day before", () => {
+    const dayBeforeChristmas = new Date(2024, 12 - 1, 24)
+    expect(daysUntilChristmas(dayBeforeChristmas)).toEqual(1);
+  });
+
+
+  test("Same day", () => {
+    const xMas = new Date(2024, 12 - 1, 25)
+    expect(daysUntilChristmas(xMas)).toEqual(0);
+  });
+
+  test("Next day", () => {
+    const dayAfterChristmas = new Date(2024, 12 - 1, 26)
+    expect(daysUntilChristmas(dayAfterChristmas)).toEqual(364);
+  });
+
+  test("Next day - of the past year", () => {
+    const dayBeforeChristmas = new Date(2023, 12 - 1, 26)
+    expect(daysUntilChristmas(dayBeforeChristmas)).toEqual(730);
+  });
+
+  test("Future date", () => {
+    const dayBeforeChristmas = new Date(2025, 12 - 1, 26)
+    expect(daysUntilChristmas(dayBeforeChristmas)).toEqual(-1);
   });
 });
