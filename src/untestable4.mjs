dcfdc508e4e0e9710ee3812e2200cc44eca8dcd1
Artjom Savin <artjom.savin@nordhealth.com>
2024-04-01 21:31:05 +0300

    Refactor db class to be not a singleton

--- src/untestable4.mjs
+++ src/untestable4.mjs
@@ -2,38 +2,40 @@ import argon2 from "@node-rs/argon2";
 import pg from "pg";
 
 export class PostgresUserDao {
-  static instance;
-
-  constructor (env = 'main') {
+  constructor(env = "main") {
     const pool = {
-      user: process.env.PGUSER,
-      host: process.env.PGHOST,
-      database: env === 'main' ? process.env.PGDATABASE : 'test-database',
-      password: process.env.PGPASSWORD,
-      port: process.env.PGPORT,
-    }
+      user: process.env.PGUSER || "untestable",
+      host: process.env.PGHOST || "localhost",
+      database: env === "main" ? process.env.PGDATABASE : "untestable",
+      password: process.env.PGPASSWORD || "secret",
+      port: process.env.PGPORT || 5432,
+    };
+    console.log(pool, " ? ? ? ? ", env);
     this.db = new pg.Pool(pool);
   }
 
-  static getInstance(env = 'main') {
-    if (!this.instance) {
-      this.instance = new PostgresUserDao(env);
-    }
+  /**
+   * Another issue that I have noticed now is that it is actually a singleton :_)
+   */
+  // static getInstance(env = "main") {
+  //   if (!this.instance) {
+  //     this.instance = new PostgresUserDao(env);
+  //   }
 
-    return this.instance;
-  }
+  //   return this.instance;
+  // }
 
   /**
- * The main problem withing the calls is that the DB pool is hardcoded to accept only
- * one user/ db/ schema, etc. This means that our dev (or prod) DB is used for testing as well.
- * To not destroy the DB data, we would need to create a separate DB or at least schema for testing
- * and pass it as a variable.
- *
- * Secondly, there is no deletion function that is needed for the clean up of the DB after the test.
- * Copying the same thing as in the untestable4.test.mjs. There could be a whole DB class created
- * for the robust clean up of the test data, however, for simplicity sake just a deletion function
- * of all users will be added to the PostgresUserDao class.
- */
+   * The main problem withing the calls is that the DB pool is hardcoded to accept only
+   * one user/ db/ schema, etc. This means that our dev (or prod) DB is used for testing as well.
+   * To not destroy the DB data, we would need to create a separate DB or at least schema for testing
+   * and pass it as a variable.
+   *
+   * Secondly, there is no deletion function that is needed for the clean up of the DB after the test.
+   * Copying the same thing as in the untestable4.test.mjs. There could be a whole DB class created
+   * for the robust clean up of the test data, however, for simplicity sake just a deletion function
+   * of all users will be added to the PostgresUserDao class.
+   */
   // db = new pg.Pool({
   //   user: process.env.PGUSER,
   //   host: process.env.PGHOST,
@@ -51,13 +53,18 @@ export class PostgresUserDao {
   }
 
   async getById(userId) {
-    const { rows } = await this.db.query(
-      `select user_id, password_hash
-       from users
-       where user_id = $1`,
-      [userId]
-    );
-    return rows.map(this.#rowToUser)[0] || null;
+    console.log(this.db);
+    try {
+      const { rows } = await this.db.query(
+        `select user_id, password_hash
+         from users
+         where user_id = $1`,
+        [userId],
+      );
+      return rows.map(this.#rowToUser)[0] || null;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async save(user) {
@@ -66,13 +73,22 @@ export class PostgresUserDao {
        values ($1, $2)
        on conflict (user_id) do update
            set password_hash = excluded.password_hash`,
-      [user.userId, user.passwordHash]
+      [user.userId, user.passwordHash],
     );
   }
 }
 
+/**
+ * Another problem I believe is that the instance here is defined within the Service.
+ * We could use dependency injection to pass the Dao to the constructor that would define
+ * which connection is used
+ */
 export class PasswordService {
-  users = PostgresUserDao.getInstance();
+  users;
+
+  constructor(users) {
+    this.users = users;
+  }
 
   async changePassword(userId, oldPassword, newPassword) {
     const user = await this.users.getById(userId);
--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,22 +1,26 @@
-import { afterEach, beforeEach, describe, test } from "vitest";
-import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+import { afterAll, describe, expect, test } from "vitest";
+import { PostgresUserDao } from "../src/untestable4.mjs";
 
 describe("Untestable 4: enterprise application", () => {
-  let service;
-  beforeEach(() => {
-    service = new PasswordService();
-  });
-
   /**
    * Another issue is that test itself. There is no clean up function called.
    * This could be handled both on the PSQL UserDao level, but also could be a part of
    * some other helper class.
    */
-  afterEach(() => {
-    PostgresUserDao.getInstance().close();
+  afterAll(() => {
+    const userDao = new PostgresUserDao("test");
+    userDao.close();
   });
 
-  test("todo", async () => {
-    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  test("Check that the test instance can be established", async () => {
+    const userDao = new PostgresUserDao("test");
+    const client = await userDao.db.connect();
+    expect(client).toBeTruthy();
+  });
+
+  test.skip("User table can be created", async () => {
+    const userDao = new PostgresUserDao("test");
+    const user1 = await userDao.getById(1);
+    console.log(user1);
   });
 });
